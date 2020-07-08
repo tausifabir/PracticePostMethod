c272949eaae1208fcf60fc47ad79236d73863b8f
Artificial Soft <tausifabir94@gmail.com>
2020-07-08 17:00:23 +0600

    new layout

--- app/src/main/java/com/example/cricketapiretrofit/Activity/RegistrationActivity.java
+++ app/src/main/java/com/example/cricketapiretrofit/Activity/RegistrationActivity.java
@@ -29,7 +29,7 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration2);
+        setContentView(R.layout.activity_registration3);
 
         Intent intent = getIntent();
 
